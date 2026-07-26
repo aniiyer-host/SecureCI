8be355fb92fc0ea4968995f0afcb0c3779764443
aniiyer-host <aniketiyer4@gmail.com>
2026-07-26 09:57:35 +0530

    webhook testing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to SecureCI!");
+    res.send("Welcome to SecureCI! now using webhooks");
 });
 
 app.listen(3000, () => {
